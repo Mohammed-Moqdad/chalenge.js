8570e5c90a97a44c74d1a92c11aed440e6d03866
Mohammed-Moqdad <125085516+Mohammed-Moqdad@users.noreply.github.com>
2023-04-08 02:26:59 +0300

    Update chaleng2.js

--- chaleng2 js/chaleng2.js
+++ chaleng2 js/chaleng2.js
@@ -1,13 +1,37 @@
-function majorityElement(n) {
-    const c = {};
-    const majorityThreshold = Math.floor(n.length / 2);
-    
-    for (let i = 0; i < n.length; i++) {
-      const nu = n[i];
-      c[nu = (c[nu] || 0) + 1;
-      if (c[nu] > majorityThreshold) {
-        return n;
-      }
+function findMajority(arr)
+{   
+    let n = arr.length;
+    let bool= true;
+    let bool2= false;
+    for (let i = 0; i < array.length; i++) {
+        if ((-1000000000) <= arr[i] <= 1000000000) {
+            continue;
+        }else{
+            bool=false;
+            break;
+        }  
     }
-  }
+    if(bool && 1 <= n <= 5*10000){
+        for(let j = 0; j < n; j++) 
+        {
+            let count = 0;
+            for(let k = 0; k < n; k++) 
+                {
+                if (arr[j] === arr[k]){
+                    count++;
+                }
+                if (count>n/2) {
+                    console.log("the majority elemen is"+arr[j]);
+                    bool2=false;
+                    break;
+                }
+            }
+            if (bool2) {
+                break;
+            }
+        }
+    }else{
+        console.log("doesn't meet the recourment")
+    }
+}
   
